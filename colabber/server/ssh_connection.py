f6b7a33badfd02292d21d47315caa204a9733b17
Gábor Vecsei <vecseigabor.x@gmail.com>
2020-01-06 19:19:33 +0100

    ssh custom password and ngrok authtoken can be passed to the ssh port forward fnc

--- colabber/server/ssh_connection.py
+++ colabber/server/ssh_connection.py
@@ -15,7 +15,7 @@ def _generate_random_str(length: int):
     return rnd_str
 
 
-def _run_cmd(cmd: str, in_bg:bool=False):
+def _run_cmd(cmd: str, in_bg: bool = False):
     # args = shlex.split(cmd)
     # TODO: do not use shell=True
     p = subprocess.Popen(cmd, shell=True)
@@ -28,14 +28,15 @@ def _setup_sshd():
     _run_cmd("apt-get install pwgen > /dev/null")
 
 
-def _set_root_pwd_random() -> tuple:
-    pwd = _generate_random_str(30)
-    _run_cmd("echo root:{0} | chpasswd".format(pwd))
+def _set_root_pwd(ssh_pwd: str) -> tuple:
+    username = "root"
+    if ssh_pwd is None:
+        ssh_pwd = _generate_random_str(30)
+    _run_cmd("echo {0}:{1} | chpasswd".format(username, ssh_pwd))
     _run_cmd(" mkdir -p /var/run/sshd")
     _run_cmd('echo "PermitRootLogin yes" >> /etc/ssh/sshd_config')
     _run_cmd('echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config')
-
-    return "root", pwd
+    return username, ssh_pwd
 
 
 def _run_sshd():
@@ -47,22 +48,23 @@ def _download_ngrok():
     _run_cmd('unzip -qq -n ngrok-stable-linux-amd64.zip')
 
 
-def _setup_ngrok():
-    authtoken = getpass.getpass()
-    _run_cmd('./ngrok authtoken {0}'.format(authtoken))
+def _setup_ngrok(ngrok_authtoken: str):
+    _run_cmd('./ngrok authtoken {0}'.format(ngrok_authtoken))
     _run_cmd("./ngrok tcp 22", in_bg=True)
 
 
-def setup_ssh_port_forwarding():
+def setup_ssh_port_forwarding(ssh_pwd: str = None, ngrok_authtoken: str = None):
     _setup_sshd()
-    username, password = _set_root_pwd_random()
+    username, password = _set_root_pwd(ssh_pwd)
     print("[*] Username: {0}".format(username))
     print("[*] Password (Use this for your SSH connection): {0}".format(password))
     _run_sshd()
     _download_ngrok()
-    print("Get your authtoken from https://dashboard.ngrok.com/auth")
-    print("Please enter you authtoken below:")
-    _setup_ngrok()
+    if ngrok_authtoken is None:
+        print("Get your authtoken from https://dashboard.ngrok.com/auth")
+        print("Please enter you authtoken below:")
+        ngrok_authtoken = getpass.getpass()
+    _setup_ngrok(ngrok_authtoken)
     print("SSH Port Forwarding is setup up via Ngrok")
     print("You can use it like: ssh root@0.tcp.ngrok.io -p [ngrok_port]")
 
